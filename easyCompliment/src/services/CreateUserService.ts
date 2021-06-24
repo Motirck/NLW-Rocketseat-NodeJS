8065449d502b866f88d154f12187e875c7e00d0d
Ricardo Alves Paula <ricardo.alves.p001@gmail.com>
2021-06-24 10:47:20 -0300

    Admin default when creating an user set with false

--- easyCompliment/src/services/CreateUserService.ts
+++ easyCompliment/src/services/CreateUserService.ts
@@ -11,7 +11,7 @@ interface IUserRequest {
 
 class CreateUserService {
 
-    async execute({ name, email, admin, password }: IUserRequest) {
+    async execute({ name, email, admin = false, password }: IUserRequest) {
         const userRepository = getCustomRepository(UserRepository);
 
         if (!email)
